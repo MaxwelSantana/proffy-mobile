c562bea2d346140a4849de3aca74700590a62031
MaxwelSantana <maxwelsant96@icloud.com>
2020-08-13 08:02:47 -0300

    added Page header

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 import styles from './styles';
+import PageHeader from '../../components/PageHeader';
 
 function Favorites() {
     return (
-        <View style={styles.container}></View>
+        <View style={styles.container}>
+            <PageHeader title="Meus proffys favoritos" />
+        </View>
     );
 }
 
